01ad7e50b5c18d02e11f76c66262219b3cabb77f
Patrick Frampton <patrick.frampton@gmail.com>
2017-03-18 15:19:13 -0400

    Get working validation

--- bin/validate.rb
+++ bin/validate.rb
@@ -1,33 +1,49 @@
 #!/usr/bin/env ruby
 require "optparse"
+require "pp"
+require "bundler"
+require 'json-schema'
 
-options = {
-  filename: "drive_forward.json",
-  schema: "schema/autonomous-schema.json"
-}
+def get_options
+  options = {
+    filename: "drive_forward.json",
+    schema: "schema/autonomous-schema.json"
+  }
 
-ARGV.options do |opts|
-	opts.banner = "Usage:  #{File.basename(\$PROGRAM_NAME)} [OPTIONS]"
+  ARGV.options do |opts|
+  	opts.banner = "Usage:  #{File.basename($PROGRAM_NAME)} [OPTIONS]"
 
-	opts.separator ""
-	opts.separator "Specific Options:"
+  	opts.separator ""
+  	opts.separator "Specific Options:"
 
-  opts.on( "-f", "--filename FILENAME") do |filename|
-    options[:filename] = filename
+    opts.on( "-f", "--filename FILENAME") do |filename|
+      options[:filename] = filename
+    end
+
+  	opts.separator "Common Options:"
+
+  	opts.on( "-h", "--help",
+  	         "Show this message." ) do
+  		puts opts
+  		exit
+  	end
+
+  	begin
+  		opts.parse!
+  	rescue
+  		puts opts
+  		exit
+  	end
   end
-
-	opts.separator "Common Options:"
-
-	opts.on( "-h", "--help",
-	         "Show this message." ) do
-		puts opts
-		exit
-	end
-
-	begin
-		opts.parse!
-	rescue
-		puts opts
-		exit
-	end
+  options
 end
+
+def main
+  options = get_options
+
+  errs = JSON::Validator.fully_validate(options[:schema], options[:filename])
+
+  pp errs
+end
+
+main
